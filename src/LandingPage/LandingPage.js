36622cad9a42a8830569ba47febc3bbe708ed9b9
RAJESH183766 <rajesh.wrathcode@gmail.com>
2026-03-08 15:44:12 +0530

    landing page update

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -684,7 +684,7 @@ function LandingPage() {
         <h2 className='heading_h2'>Trending Games</h2>
         <div className='game_items_video'>
           {(() => {
-            const trendingCategories = ['Crash Type', 'Dragon Tiger', '000', 'Baccarat', 'Roulette', 'Teen Patti'];
+            const trendingCategories = ['Crash Type', 'Dragon Tiger', 'Chicken Games', 'Baccarat', 'Roulette', 'Teen Pati'];
             return showTrendingVideos
               ? trendingVideos.map((src, i) => {
                   const category = trendingCategories[i] ?? 'lobby';
